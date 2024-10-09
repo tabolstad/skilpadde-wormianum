913e7fcc731ae3e807f131d873ea7a69dcf26bbb
Tim Bolstad <tim@skilpaddelabs.com>
2024-10-09 08:53:10 -0400

    Initial Commit

---
+++ Wormianum/WormianumApp.swift
@@ -0,0 +1,17 @@
+//
+//  WormianumApp.swift
+//  Wormianum
+//
+//  Created by Timothy Bolstad on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct WormianumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WormianumTests/WormianumTests.swift
@@ -0,0 +1,16 @@
+//
+//  WormianumTests.swift
+//  WormianumTests
+//
+//  Created by Timothy Bolstad on 10/9/24.
+//
+
+import Testing
+
+struct WormianumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
