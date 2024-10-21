696e4e34fe6aeec4031dd8deb4b2c27e235cb47c
Tim Bolstad <tim@skilpaddelabs.com>
2024-10-21 11:23:13 -0400

    Add top level navigation.

---
+++ Wormianum/SwiftUI/Containers/ContainersView.swift
@@ -0,0 +1,31 @@
+//
+//  ContainersView.swift
+//  Wormianum
+//
+//  Created by Timothy Bolstad on 10/21/24.
+//
+
+import SwiftUI
+
+/*
+ Demonstrates usage of SwiftUI container views.
+
+ References:
+ WWDC24 - Demystify SwiftUI container
+ https://developer.apple.com/videos/play/wwdc2024/10146/
+ Paul Hudson
+ https://www.hackingwithswift.com/quick-start/swiftui/how-to-position-and-style-subviews-that-come-from-a-different-view
+*/
+
+struct ContainersView: View {
+
+    @State var isOn = true
+
+    var body: some View {
+        Text("containers")
+    }
+}
+
+#Preview {
+    ContainersView()
+}
